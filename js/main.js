ad2cf82a6cff0bb05b53d986061fd30f5884c1b3
LadyViolet <Laura@zeilbeck-metallbau.de>
2023-04-17 23:02:20 +0200

    added eventListeners

--- js/main.js
+++ js/main.js
@@ -3,15 +3,30 @@
 
 const followUs = document.querySelector('#follow-us');
 const collapseOnSmallScreens = document.querySelectorAll('.collapseOnSmall');
+const break900 = window.matchMedia("(max-width: 900px)");
+const break768 = window.matchMedia("(max-width: 768px)");
 
-window.onresize = function() {
-        //breakpoint 900px
-    if (window.innerWidth <= 900) followUs.classList.add('collapse');
-    else followUs.classList.remove('collapse');
-    //breakpoint 768px medium screens
-    if (window.innerWidth < 768) collapseOnSmallScreens.forEach((element) => {
-    element.classList.add('collapse');})
-    else collapseOnSmallScreens.forEach((element) => {
-        element.classList.remove('collapse');})
+
+function collapseItems900(break900) {
+    //breakpoint 900px
+    if (break900.matches) {
+        followUs.classList.add('collapse');  
+    } else {
+        followUs.classList.remove('collapse');
+    }
 };
 
+function collapseItems768(break768) {
+    if (break768.matches) {
+        collapseOnSmallScreens.forEach((element) => {
+        element.classList.add('collapse');})
+    } else {
+        collapseOnSmallScreens.forEach((element) => {
+            element.classList.remove('collapse');})  
+    } 
+};
+
+collapseItems900(break900); 
+collapseItems768(break768); 
+break900.addListener(collapseItems900);
+break768.addListener(collapseItems768);
