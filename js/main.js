05a7b38d0f7e0304cbd88b40414940ca0bfaa70f
LadyViolet <Laura@zeilbeck-metallbau.de>
2023-04-17 21:59:23 +0200

    added footer-items collapse behaviour

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-//TODO
+//collapsing behaviour of containers
+//default bootstrap breakpoint: >576px
+
+const followUs = document.querySelector('#follow-us');
+const collapseOnSmallScreens = document.querySelectorAll('.collapseOnSmall');
+
+window.onresize = function() {
+        //breakpoint 900px
+    if (window.innerWidth <= 900) followUs.classList.add('collapse');
+    else followUs.classList.remove('collapse');
+    //breakpoint 768px medium screens
+    if (window.innerWidth < 768) collapseOnSmallScreens.forEach((element) => {
+    element.classList.add('collapse');})
+    else collapseOnSmallScreens.forEach((element) => {
+        element.classList.remove('collapse');})
+};
+
